88651d6078c425ca8bfc63c20740f0d6b17ecbb9
Pablo Adell <adell13pablo@gmail.com>
2018-11-15 10:55:57 +0100

    Control de argumentos metidos

--- asteroids.cpp
+++ asteroids.cpp
@@ -8,13 +8,31 @@
 
 
   int main (int argc, char **argv){
-
   vector <asteroid> asteroids;//Vector of asteroids
-  int asteroid_number = 0;
+  int num_asteroids;
+  int num_iterations;
+  int num_planets;
+  int seed;
 
+  if(argc != 5){
+    cout << " nasteroids-seq: Wrong arguments " << "\n";
+        cout << " Correct use: " << "\n";
+            cout << " nasteroids-seq num_asteroids num_iterations num_planets seed " << "\n";
+return -1;
+  }
+else{
   cout << "Enter number of asteroids " << "\n";
-  cin >> asteroid_number;
-    for( int i = 0; i< asteroid_number; i++){
+  cin >> num_asteroids;
+  cout << "Enter number of iterations " << "\n";
+  cin >> num_iterations;
+  cout << "Enter number of planets " << "\n";
+  cin >> num_planets;
+  cout << "Enter the seed " << "\n";
+  cin >> seed;
+
+  cout << "Asteroids: " << num_asteroids << " Iterations: " << num_iterations
+  << " Planets: " << num_planets << " Seed: " << seed << " \n";
+    for( int i = 0; i< num_asteroids; i++){
       asteroids.push_back(asteroid()); //Adding one element to the vector
       asteroids[i].mass = 3 * i + 5;
       cout << "Mass for asteroid " << i << " is " << asteroids[i].mass << "\n";
@@ -25,4 +43,5 @@
 
 
     return 0;
+  }
   }
