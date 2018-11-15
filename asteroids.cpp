96a6c527b303e75e91f606a4eeeaaeaef4d61299
rluque8 <rafarod8@gmail.com>
2018-11-15 11:13:12 +0100

    Argumentos leidos correctamente menos el error case

--- asteroids.cpp
+++ asteroids.cpp
@@ -9,39 +9,50 @@
 
   int main (int argc, char **argv){
   vector <asteroid> asteroids;//Vector of asteroids
-  int num_asteroids;
-  int num_iterations;
-  int num_planets;
-  int seed;
+  int num_asteroids=atoi(argv[1]);
+  int num_iterations=atoi(argv[2]);
+  int num_planets=atoi(argv[3]);
+  int seed=atoi(argv[4]);
+
+
+  // int num_asteroids;
+  // int num_iterations;
+  // int num_planets;
+  // int seed;
 
   if(argc != 5){
     cout << " nasteroids-seq: Wrong arguments " << "\n";
         cout << " Correct use: " << "\n";
             cout << " nasteroids-seq num_asteroids num_iterations num_planets seed " << "\n";
-return -1;
+    return -1;
   }
 else{
-  cout << "Enter number of asteroids " << "\n";
-  cin >> num_asteroids;
-  cout << "Enter number of iterations " << "\n";
-  cin >> num_iterations;
-  cout << "Enter number of planets " << "\n";
-  cin >> num_planets;
-  cout << "Enter the seed " << "\n";
-  cin >> seed;
+  for(int i=0;i<num_asteroids;i++){
+    asteroids.push_back(asteroid());
+    asteroids[i].setPositionAsteroid(&asteroids[i]);
+    cout << "Position of asteroid " << asteroids[i].x << endl;
+  }
+  // cout << "Enter number of asteroids " << "\n";
+  // cin >> num_asteroids;
+  // cout << "Enter number of iterations " << "\n";
+  // cin >> num_iterations;
+  // cout << "Enter number of planets " << "\n";
+  // cin >> num_planets;
+  // cout << "Enter the seed " << "\n";
+  // cin >> seed;
 
   cout << "Asteroids: " << num_asteroids << " Iterations: " << num_iterations
   << " Planets: " << num_planets << " Seed: " << seed << " \n";
-    for( int i = 0; i< num_asteroids; i++){
-      asteroids.push_back(asteroid()); //Adding one element to the vector
-      asteroids[i].mass = 3 * i + 5;
-      cout << "Mass for asteroid " << i << " is " << asteroids[i].mass << "\n";
-      asteroids[i].setPositionAsteroid(&asteroids[i]);
-      //asteroids[i].x = 8;
-      cout << "Position of asteroid " << asteroids[i].x << endl;
-    }
+    // for( int i = 0; i< num_asteroids; i++){
+    //   asteroids.push_back(asteroid()); //Adding one element to the vector
+    //   asteroids[i].mass = 3 * i + 5;
+    //   cout << "Mass for asteroid " << i << " is " << asteroids[i].mass << "\n";
+    //   asteroids[i].setPositionAsteroid(&asteroids[i]);
+    //   //asteroids[i].x = 8;
+    //   cout << "Position of asteroid " << asteroids[i].x << endl;
+    // }
 
 
     return 0;
-  }
+    }
   }
